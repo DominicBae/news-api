32d4467410995f61ec569644021e01278c8421cc
DominicBae <baehope823@hanmail.net>
2024-07-09 23:47:18 +0900

    second commit

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const API_KEY = `32edf3a924aa466aa0c04340e74dcec2`
 
 const getLatestNews = () => {
-    const url = new URL(`https://dominic-times.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`);
-    console.log("uuu", url);
+    const url = new URL(`https://popvibe.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`);
+    console.log(url);
     const response = fetch(url);
 };
 
