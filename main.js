61937e6617ef724019e9aa1d7c39c6873c4f35d3
DominicBae <baehope823@hanmail.net>
2024-07-11 07:22:04 +0900

    6th commit

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const API_KEY = `32edf3a924aa466aa0c04340e74dcec2`;
-let news = [];
+let newsList = [];
 
 const getLatestNews = async () => {
     const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&apiKey=${API_KEY}`);
     const response = await fetch(url);
     const data = await response.json();
-    news = data.articles;
-    console.log("dddd", news);
+    newsList = data.articles;
+    render();
+    console.log("dddd", newsList);
 };
-
-getLatestNews();
 
 const hamburger = document.getElementById('hamburger');
 const sidebar = document.getElementById('sidebar');
@@ -44,3 +43,28 @@ const handleResize = () => {
 
 window.addEventListener('resize', handleResize);
 window.addEventListener('load', handleResize);
+
+
+const render = () => {
+    const newsHTML = newsList.map(news => `<div class="row news pt-3 pb-3">
+        <div class="col-lg-4">
+            <img class="news-img-size"
+                src=${news.urlToImage}
+                >
+        </div>
+        <div class="col-lg-8 mt-2">
+            <h2>${news.title}</h2>
+            <p>
+                ${news.description}
+            </p>
+            <div>
+               ${news.source.name} * ${news.publishedAt}
+            </div>
+        </div>
+    </div>`)
+        .join("");
+
+    document.getElementById('news-board').innerHTML = newsHTML
+}
+
+getLatestNews();
