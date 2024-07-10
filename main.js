23d5564019542acf5ed9cb97aa470ba8b781b302
DominicBae <baehope823@hanmail.net>
2024-07-10 23:12:09 +0900

    3rd commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY = `32edf3a924aa466aa0c04340e74dcec2`
 let news = []
 
 const getLatestNews = async () => {
-    const url = new URL(`https://popvibe.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&apiKey=${API_KEY}`);
     const response = await fetch(url);
     const data = await response.json();
     news = data.articles;
