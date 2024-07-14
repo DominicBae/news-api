2d378668c74576dac422313e70323a24bd0b3967
DominicBae <baehope823@hanmail.net>
2024-07-14 17:31:06 +0900

    finish

--- main.js
+++ main.js
@@ -159,28 +159,31 @@ const paginationRender = () => {
 
   const firstPage = lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
 
-  let paginationHTML = `<li class="page-item" onclick="moveToPage(${page-1})"><a class="page-link" href="#">Previous</a></li>`
+  let paginationHTML = '';
 
+  if (pageGroup > 1) {
+    paginationHTML += `
+      <li class="page-item" onclick="moveToPage(1)"><a class="page-link" href="#">&laquo;</a></li>
+      <li class="page-item" onclick="moveToPage(${page-1})"><a class="page-link" href="#">&lt;</a></li>`;
+  }
 
   for (let i = firstPage; i <= lastPage; i++) {
     paginationHTML += `<li class="page-item ${i === page ? "active" : ""}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
   }
-  paginationHTML += `<li class="page-item" onclick="moveToPage(${page+1})"><a class="page-link" href="#">Next</a></li>`
-  document.querySelector(".pagination").innerHTML = paginationHTML;
 
-  // <nav aria-label="Page navigation example">
-  //   <ul class="pagination">
-  //     {/* <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-  //   <li class="page-item"><a class="page-link" href="#">1</a></li>
-  //   <li class="page-item"><a class="page-link" href="#">2</a></li>
-  //   <li class="page-item"><a class="page-link" href="#">3</a></li>
-  //   <li class="page-item"><a class="page-link" href="#">Next</a></li> */}
-  //   </ul>
-  // </nav>
+  if (pageGroup * groupSize < totalPages) {
+    paginationHTML += `
+      <li class="page-item" onclick="moveToPage(${page+1})"><a class="page-link" href="#">&gt;</a></li>
+      <li class="page-item" onclick="moveToPage(${totalPages})"><a class="page-link" href="#">&raquo;</a></li>`;
+  }
+
+  document.querySelector(".pagination").innerHTML = paginationHTML;
 }
 
 const moveToPage = (pageNum) => {
-  console.log("movetopage", pageNum);
+  const totalPages = Math.ceil(totalResult / pageSize);
+  if (pageNum < 1) pageNum = 1;
+  if (pageNum > totalPages) pageNum = totalPages;
   page = pageNum;
   getNews();
 }
